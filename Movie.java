f515bc98eda3e20eb75e033b4f3c652e24ab1326
ezralevy <55504729+ezralevy@users.noreply.github.com>
2019-10-11 09:35:39 -0700

    Add files via upload

---
+++ Movie.java
@@ -0,0 +1,64 @@
+
+
+public class Movie
+{
+    private String title;
+    private String genre;
+    private double lengthInMin;
+    private String starActor;
+    private double rottenRating;
+    
+    
+    public Movie(String titl, String gen, double leng, String star, double rot)
+    {
+    title = titl;
+    genre = gen;
+    lengthInMin= leng;
+    starActor=star;
+    rottenRating= rot;
+ }
+ public void setTitle(String t)
+    {
+    title = t;
+    }
+ public string getTitle()
+ {
+    return title;
+ }  
+ public void setGenre(String t)
+    {
+    genre = g;
+    }
+ public string getGenre()
+ {
+    return genre;
+ }  public void setLenth(String t)
+    {
+    lengthInMin = l;
+    }
+ 
+ public string getTitle()
+ {
+    return title;
+ }  
+ public void setTitle(String t)
+    {
+    title = t;
+    }
+ public string getTitle()
+ {
+    return title;
+ }  
+ public void setTitle(String t)
+    {
+    title = t;
+    }
+ public string getTitle()
+ {
+    return title;
+ }  
+ return title;
+  
+ }  
+
+  
